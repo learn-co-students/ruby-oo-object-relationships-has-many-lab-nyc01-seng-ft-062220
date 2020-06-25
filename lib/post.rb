ad8ec398301c03ae0373ad3234a7247e988a56d1
dukhyunko93 <dukhyunko93@gmail.com>
2020-06-24 22:09:03 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+
+    attr_accessor :title, :author
+
+        @@all = []
+
+    def initialize(title, author = nil)
+        @title = title
+        @author = author
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        self.author == nil ? self.author : self.author.name
+    end
+
+
+end
